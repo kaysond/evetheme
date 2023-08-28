802de9001658176ae7ca80da580d9480cb9d8312
Aram Akhavan <github@aram.nubmail.ca>
2023-08-27 21:18:09 -0700

    test case for theme debug

--- eve/pages/homepage/MainBanner.jsx
+++ eve/pages/homepage/MainBanner.jsx
@@ -4,7 +4,7 @@ import "./MainBanner.scss";
 function MainBanner() {
   return (
     <div className="main-banner-home flex items-center">
-      <div className="page-width grid grid-cols-1 md:grid-cols-2 gap-2">
+      <div className="page-width grid grid-cols-1 md:grid-cols-2 gap-2 mt-auto mb-16">
         <div className="text-center md:text-left px-2 ">
           <h2 className="h1 ">Unleash Your Inner Style Icon</h2>
           <p>Discover the Latest Trends and Elevate Your Wardrobe Today</p>
